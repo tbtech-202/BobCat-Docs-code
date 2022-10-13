906cafa26663c257940bd1a232e66d85d2c68245
TBTech205 <48381745+TBTech205@users.noreply.github.com>
2022-10-13 10:03:44 +0100

    Update DefaultLayout.tsx

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -16,9 +16,7 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ title, description, child
             <Head>
                 <title>{ title } - { site.tagline }</title>
                 <meta name="description" content={ description } />
-                <meta property="og:description" content="Server Manager is a unique Discord bot that gives server moderators and admins easy and reliable ways to moderate servers, create interactive giveaways, create polls to vote on, and more. Let Server Manager do the hard work for you." />
-                <meta property="og:title" content="Server Manager" />
-                <meta property="og:image" content="https://imgur.com/BHyNRD2.png"/>
+                <meta property="og:title" content="BobCat Bot" />
                 <meta name="twitter:card" content="summary_large_image"/>
                 <meta name="theme-color" content="#5865F2" />
             </Head>
